c0a005fa710df8596dc13aa9cfdbacd33f7b51a0
Meta Luo <a598528009@qq.com>
2016-04-10 15:31:48 +0800

    githubpage的修改
    对githubpage的首页进行了一些修改

--- mystyle.js
+++ mystyle.js
@@ -18,32 +18,12 @@ $(document).ready(function(){
         $(".tooltip").animate({"opacity":0,"top":100},300)
        })
     })
-       $(function(){
-           $(".lefthead a").hover(function(){
-               var title=$(this).attr("data-title");
-               $(".tooltip em").text(title)
-               var pos=$(this).offset().left;
-               $(".tooltip").css({"left":pos-450+"px",top:"-400px"}).animate({"opacity":1,"top":-200},300)
-           },function(){
-               $(".tooltip").animate({"opacity":0,"top":-180},300)
-           })
-       })
-    $(function(){
-        $(".leftguide a").hover(function(){
-            var title=$(this).attr("data-title");
-            $(".tooltip em").text(title)
-            var pos=$(this).offset().top;
-            $(".tooltip").css({"top":-300+pos+"px"}).animate({"opacity":1,"left":"-400px"},300)
-        },function(){
-            $(".tooltip").animate({"opacity":0,"left":"-350px"},300)
-        })
-    })
     $(function(){
         $(".bigtitle a").hover(function(){
             var title=$(this).attr("data-title");
             $(".tooltip em").text(title)
             var pos=$(this).offset().left;
-            $(".tooltip").css({"left":pos-400+"px"}).animate({"opacity":1,"top":250},300)
+            $(".tooltip").css({"left":pos+"px"}).animate({"opacity":1,"top":250},300)
         },function(){
             $(".tooltip").animate({"opacity":0,"top":200},300)
         })
